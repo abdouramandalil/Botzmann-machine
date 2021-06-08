b20c3579b47179174fdc79f39a277cb5625671eb
Abdouraman Bouba Dalil <59530942+abdouramandalil@users.noreply.github.com>
2021-06-08 12:53:52 +0100

    Update right_code_boltzmann_machine.py

--- right_code_boltzmann_machine.py
+++ right_code_boltzmann_machine.py
@@ -1,3 +1,4 @@
+#1 - import libraries
 import numpy as np
 import pandas as pd
 import torch
@@ -6,20 +7,20 @@ import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
-
+#2 - import the dataset
 movies=pd.read_csv("ml-1m/movies.dat",sep="::",header=None,engine="python",encoding="latin-1")
 users=pd.read_csv("ml-1m/users.dat",sep="::",header=None,engine="python",encoding="latin-1")
 ratings=pd.read_csv("ml-1m/ratings.dat",sep="::",header=None,engine="python",encoding="latin-1")
-
+#3 - training set and test set
 training_set=pd.read_csv("ml-100k/u1.base",delimiter="\t")
 training_set=np.array(training_set,dtype="int")
 
 test_set=pd.read_csv("ml-100k/u1.test",delimiter="\t")
 test_set=np.array(test_set,dtype="int")
-
+#4 - find number of users and number of movies
 nb_users = int(max(max(training_set[:,0]), max(test_set[:,0])))
 nb_movies = int(max(max(training_set[:,1]), max(test_set[:,1])))
-
+#5 - Define a function to convert datas
 def convert(data):
     new_data=[]
     for id_users in range(1,nb_users+1):
@@ -29,7 +30,7 @@ def convert(data):
         ratings[id_movies-1]=id_ratings
         new_data.append(list(ratings))
     return new_data
-
+#6 -convert datas
 training_set=convert(training_set)
 test_set=convert(test_set)
 
@@ -48,7 +49,7 @@ test_set[test_set==1] ==0
 test_set[test_set==2] ==0
 test_set[test_set>=3] ==1   
 
-
+#7 - construct the architecture of the botzmann machine
 class RBM():
     def __init__(self,nv,nh):
         self.W=torch.randn(nh,nv)
@@ -68,7 +69,7 @@ class RBM():
         self.W += (torch.mm(v0.t(),ph0)- torch.mm(vk.t(),phk)).t()
         self.b += torch.sum((v0-vk),0)
         self.a += torch.sum((ph0-phk),0)
-        
+#8 - Train the botzmann machine        
 nv=len(training_set[0])
 nh=100
 batch_size=100
@@ -92,7 +93,7 @@ for epochs in range(1,epochs+1):
         s +=1.
     print("epoch: "+str(epochs)+"loss: "+str(train_loss/s))        
         
-
+#9 - test the botzmann machine
 test_loss=0
 s=0.
 for id_user in range(nb_users):
